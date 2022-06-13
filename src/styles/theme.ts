9c12e45244ee240b03f93f80780c3fcb6cc47fc6
Joshua Nathaniel M <clsjoshua@gmail.com>
2022-06-14 06:19:13 +0700

    feat: create theme file for chakra ui custom theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Poppins', sans-serif`,
+    body: `'Poppins', sans-serif`,
+  },
+  components: {
+    Text: {
+      baseStyle: {
+        color: 'gray.300',
+      },
+    },
+  },
+})
+
+export default theme
