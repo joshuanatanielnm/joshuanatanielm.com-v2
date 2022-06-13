4fdf3cbfb6c2d34206621edfa3a650ada9d0cd13
Joshua Nathaniel M <clsjoshua@gmail.com>
2022-06-14 06:22:08 +0700

    feat: create root for navigation

---
+++ src/constant/routes.tsx
@@ -0,0 +1,6 @@
+export const NAVIGATION = [
+  { title: 'About', path: 'about' },
+  { title: 'Experience', path: 'experience' },
+  { title: 'Work', path: 'work' },
+  { title: 'Apparence', path: 'apparence' },
+]
