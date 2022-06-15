29ff92bdebd8f23b0f25a96509a78e3fcdab7687
Joshua Nathaniel M <clsjoshua@gmail.com>
2022-06-15 17:46:06 +0700

    feat: update list component

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Box, ListItem, Text } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import { Box, ListItem, Text, Link } from '@chakra-ui/react'
 import { format } from 'date-fns'
 import { ExperienceType } from '../pages/api/fetch'
 
@@ -10,10 +11,16 @@ export const ExperienceList = ({ value }: ExperienceListProps) => {
   return (
     <Box>
       <ListItem color='gray.300'>
-        {`${value.title} at ${value.at} (${format(
-          new Date(value.startDate),
-          'MMMM yyyy'
-        )} - ${value.endDate == null ? 'Present' : value.endDate})`}
+        <>
+          {value.title} at{' '}
+          <NextLink href={value.link || ''} passHref>
+            <Link color='white' fontWeight='medium' target='_blank'>
+              {value.at}
+            </Link>
+          </NextLink>{' '}
+          ({format(new Date(value.startDate), 'MMMM yyyy')} -{' '}
+          {value.endDate == null ? 'Present' : value.endDate})
+        </>
       </ListItem>
       <Text>{value.jobDescription}</Text>
     </Box>
