b2371ea7678af50fc3609a9eaccde487e3574516
Joshua Nathaniel M <clsjoshua@gmail.com>
2022-07-11 16:24:54 +0700

    feat: improve accessibility

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -6,43 +6,43 @@ import {
   Spacer,
   Stack,
   Text,
-} from '@chakra-ui/react'
-import React from 'react'
-import { ProjectType, TalkType } from '../pages/api/fetch'
-import NextLink from 'next/link'
-import TextMarkdown from './text-markdown'
-import { format } from 'date-fns'
-import { LinkExternal16 } from '@chakra-icons/octicons'
-import { Github } from '@chakra-icons/bootstrap'
-import { Icon } from './icon'
-import Image from 'next/image'
+} from "@chakra-ui/react";
+import React from "react";
+import { ProjectType, TalkType } from "../pages/api/fetch";
+import NextLink from "next/link";
+import TextMarkdown from "./text-markdown";
+import { format } from "date-fns";
+import { LinkExternal16 } from "@chakra-icons/octicons";
+import { Github } from "@chakra-icons/bootstrap";
+import { Icon } from "./icon";
+import Image from "next/image";
 
 interface ProjectCardProps {
-  value: ProjectType
+  value: ProjectType;
 }
 
 interface AppearanceCardProps {
-  value: TalkType
+  value: TalkType;
 }
 
 export const ProjectCard = ({ value }: ProjectCardProps) => {
   return (
     <Box
       _hover={{
-        bgGradient: 'linear(to-l, #879af2, #d3208b, #fda000)',
+        bgGradient: "linear(to-l, #879af2, #d3208b, #fda000)",
       }}
-      role='group'
-      bg='gray.700'
-      borderRadius='lg'
+      role="group"
+      bg="gray.700"
+      borderRadius="lg"
       p={1}
     >
       <Stack
         spacing={4}
-        bgColor='#011627'
-        overflow='hidden'
-        borderRadius='lg'
-        direction='column'
-        height='full'
+        bgColor="#011627"
+        overflow="hidden"
+        borderRadius="lg"
+        direction="column"
+        height="full"
       >
         <Box
           _groupHover={{
@@ -50,29 +50,24 @@ export const ProjectCard = ({ value }: ProjectCardProps) => {
           }}
           opacity={0.5}
         >
-          <Image
-            src={value.image.url}
-            alt={value.image.alt}
-            width={800}
-            height={420}
-          />
+          <Image src={value.image.url} alt="" width={800} height={420} />
         </Box>
-        <Stack justifyContent='center' py={4} px={{ base: 3, md: 6 }} h='full'>
-          <Heading fontSize='2xl' pb={2}>
+        <Stack justifyContent="center" py={4} px={{ base: 3, md: 6 }} h="full">
+          <Heading fontSize="2xl" pb={2}>
             {value.name}
           </Heading>
-          <Text fontSize={{ base: 'lg', md: 'xl' }} pb={6}>
+          <Text fontSize={{ base: "lg", md: "xl" }} pb={6}>
             {value.summary}
           </Text>
           <Spacer />
           <HStack spacing={4}>
             <NextLink href={value.repositoryLink} passHref>
-              <Link target='_blank' p={2} aria-label='github repository'>
+              <Link target="_blank" p={2} aria-label="github repository">
                 <Icon title={value.name} icon={<Github boxSize={7} />} />
               </Link>
             </NextLink>
             <NextLink href={value.websiteLink} passHref>
-              <Link target='_blank' p={2} aria-label='project website'>
+              <Link target="_blank" p={2} aria-label="project website">
                 <Icon
                   title={value.name}
                   icon={<LinkExternal16 boxSize={7} />}
@@ -83,43 +78,43 @@ export const ProjectCard = ({ value }: ProjectCardProps) => {
         </Stack>
       </Stack>
     </Box>
-  )
-}
+  );
+};
 
 export const AppearanceCard = ({ value }: AppearanceCardProps) => {
   return (
     <Box
       _hover={{
-        bgGradient: 'linear(to-l, #879af2, #d3208b, #fda000)',
+        bgGradient: "linear(to-l, #879af2, #d3208b, #fda000)",
       }}
-      bg='gray.700'
-      borderRadius='lg'
+      bg="gray.700"
+      borderRadius="lg"
       p={1}
     >
       <NextLink href={value.link} passHref>
         <Link
-          target='_blank'
-          style={{ textDecoration: 'none' }}
-          role='group'
+          target="_blank"
+          style={{ textDecoration: "none" }}
+          role="group"
           aria-label={value.name}
         >
-          <Box bgColor='#011627' p={4} borderRadius='lg'>
+          <Box bgColor="#011627" p={4} borderRadius="lg">
             <Heading
-              fontSize='2xl'
+              fontSize="2xl"
               pb={2}
               _groupHover={{
-                textDecoration: 'underline',
+                textDecoration: "underline",
               }}
             >
               {value.name}
             </Heading>
             <Text mb={2}>
-              {format(new Date(value.date), 'EEEE, MMMM do yyyy')}
+              {format(new Date(value.date), "EEEE, MMMM do yyyy")}
             </Text>
             <TextMarkdown>{value.description}</TextMarkdown>
           </Box>
         </Link>
       </NextLink>
     </Box>
-  )
-}
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import type { GetStaticProps, NextPage } from 'next'
-import { NextSeo } from 'next-seo'
-import NextLink from 'next/link'
+import type { GetStaticProps, NextPage } from "next";
+import { NextSeo } from "next-seo";
+import NextLink from "next/link";
 import {
   Box,
   Heading,
@@ -11,72 +11,72 @@ import {
   Center,
   Flex,
   SlideFade,
-} from '@chakra-ui/react'
+} from "@chakra-ui/react";
 import {
   getHome,
   AllContent,
   ExperienceType,
   ProjectType,
   TalkType,
-} from './api/fetch'
-import { SOCIAL_LINKS } from '../constant/social'
-import TextMarkdown from '../components/text-markdown'
-import { ExperienceList } from '../components/list'
+} from "./api/fetch";
+import { SOCIAL_LINKS } from "../constant/social";
+import TextMarkdown from "../components/text-markdown";
+import { ExperienceList } from "../components/list";
 import {
   PageContainer,
   ContentSectionContainer,
   GridItemContainer,
   StackItemContainer,
-} from '../components/layouts/'
-import { ProjectCard, AppearanceCard } from '../components/card'
-import { Icon } from '../components/icon'
-import Image from 'next/image'
+} from "../components/layouts/";
+import { ProjectCard, AppearanceCard } from "../components/card";
+import { Icon } from "../components/icon";
+import Image from "next/image";
 
 export interface HomePageProps {
-  data: AllContent
+  data: AllContent;
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const data = (await getHome()) || []
+  const data = (await getHome()) || [];
 
   return {
     props: { data },
-  }
-}
+  };
+};
 
 const Home: NextPage<HomePageProps> = ({ data }) => {
   return (
-    <Box bgColor='#011627' color='white' minH='100vh'>
+    <Box bgColor="#011627" color="white" minH="100vh">
       <NextSeo
-        title='Joshua Nathaniel M - Frontend Developer'
-        description='I am a software engineer who likes to create user interfaces'
+        title="Joshua Nathaniel M - Frontend Developer"
+        description="I am a software engineer who likes to create user interfaces"
         openGraph={{
-          title: 'Joshua Nathaniel M - Frontend Developer',
+          title: "Joshua Nathaniel M - Frontend Developer",
           description:
-            'I am a software engineer who likes to create user interfaces',
-          url: 'https://joshuanatanielm.com/',
+            "I am a software engineer who likes to create user interfaces",
+          url: "https://joshuanatanielm.com/",
         }}
       />
 
       <PageContainer>
-        <SlideFade in offsetY='20px' delay={0.2}>
+        <SlideFade in offsetY="20px" delay={0.2}>
           <Stack
             spacing={{ base: 6, md: 4 }}
-            minH='100vh'
-            direction='column'
-            justifyContent='center'
-            id='hero'
+            minH="100vh"
+            direction="column"
+            justifyContent="center"
+            id="hero"
           >
-            <Text fontSize={{ base: '3xl', md: '4xl' }} lineHeight={10}>
+            <Text fontSize={{ base: "3xl", md: "4xl" }} lineHeight={10}>
               Hello, I`m Joshua Manuputty
             </Text>
             <Heading
-              as='h1'
-              size={{ base: '3xl', md: '4xl' }}
-              textShadow='-7px -5px #ffffff'
+              as="h1"
+              size={{ base: "3xl", md: "4xl" }}
+              textShadow="-7px -5px #ffffff"
               pb={3}
-              bgGradient='linear(to-l, #879af2, #d3208b, #fda000)'
-              bgClip='text'
+              bgGradient="linear(to-l, #879af2, #d3208b, #fda000)"
+              bgClip="text"
             >
               I build things for the web.
             </Heading>
@@ -84,8 +84,12 @@ const Home: NextPage<HomePageProps> = ({ data }) => {
             <Flex gap={6} pt={10}>
               {SOCIAL_LINKS.map((item) => (
                 <NextLink href={item.link} passHref key={item.title}>
-                  <Link target='_blank' p={2} aria-label={item.title}>
-                    <Icon title={item.title} icon={item.icon} />
+                  <Link target="_blank" p={2} aria-label={item.title}>
+                    <Icon
+                      title={item.title}
+                      icon={item.icon}
+                      aria-hidden="true"
+                    />
                   </Link>
                 </NextLink>
               ))}
@@ -95,24 +99,24 @@ const Home: NextPage<HomePageProps> = ({ data }) => {
 
         <Stack spacing={20} pt={8}>
           <ContentSectionContainer>
-            <Heading fontSize={{ base: '3xl', md: '4xl' }} as='h2' id='about'>
+            <Heading fontSize={{ base: "3xl", md: "4xl" }} as="h2" id="about">
               About
             </Heading>
             <Stack
-              direction={{ base: 'column-reverse', lg: 'row' }}
-              justifyContent='space-between'
+              direction={{ base: "column-reverse", lg: "row" }}
+              justifyContent="space-between"
               spacing={4}
             >
-              <Box w={{ base: 'full', lg: '70%' }}>
+              <Box w={{ base: "full", lg: "70%" }}>
                 <TextMarkdown>{data.home.aboutMe}</TextMarkdown>
               </Box>
               <Center>
-                <Box boxSize={{ base: 40, md: 60, lg: 72 }} position='relative'>
+                <Box boxSize={{ base: 40, md: 60, lg: 72 }} position="relative">
                   <Image
                     src={data.home.aboutImage.url}
                     alt={data.home.aboutImage.alt}
-                    layout='fill'
-                    objectFit='contain'
+                    layout="fill"
+                    objectFit="contain"
                   />
                 </Box>
               </Center>
@@ -121,28 +125,28 @@ const Home: NextPage<HomePageProps> = ({ data }) => {
 
           <ContentSectionContainer>
             <Heading
-              fontSize={{ base: '3xl', md: '4xl' }}
-              as='h2'
-              id='experience'
+              fontSize={{ base: "3xl", md: "4xl" }}
+              as="h2"
+              id="experience"
             >
               Experience
             </Heading>
             <StackItemContainer>
               <Box>
-                <Heading fontSize='2xl' as='h3' color='white' pb={2}>
+                <Heading fontSize="2xl" as="h3" color="white" pb={2}>
                   Work Experience
                 </Heading>
-                <UnorderedList fontSize='xl'>
+                <UnorderedList fontSize="xl">
                   {data.allWorkExperiences.map((v: ExperienceType) => (
                     <ExperienceList value={v} key={v.at} />
                   ))}
                 </UnorderedList>
               </Box>
               <Box>
-                <Heading fontSize='2xl' as='h3' color='white' pb={2}>
+                <Heading fontSize="2xl" as="h3" color="white" pb={2}>
                   Community Experience
                 </Heading>
-                <UnorderedList fontSize='xl'>
+                <UnorderedList fontSize="xl">
                   {data.allCommunityExperiences.map((v: ExperienceType) => (
                     <ExperienceList value={v} key={v.at} />
                   ))}
@@ -152,7 +156,7 @@ const Home: NextPage<HomePageProps> = ({ data }) => {
           </ContentSectionContainer>
 
           <ContentSectionContainer>
-            <Heading fontSize={{ base: '3xl', md: '4xl' }} as='h2' id='work'>
+            <Heading fontSize={{ base: "3xl", md: "4xl" }} as="h2" id="work">
               Some Things I’ve Built
             </Heading>
             <GridItemContainer>
@@ -164,9 +168,9 @@ const Home: NextPage<HomePageProps> = ({ data }) => {
 
           <ContentSectionContainer>
             <Heading
-              fontSize={{ base: '3xl', md: '4xl' }}
-              as='h2'
-              id='apparence'
+              fontSize={{ base: "3xl", md: "4xl" }}
+              as="h2"
+              id="apparence"
             >
               Recent Appearances
             </Heading>
@@ -178,7 +182,7 @@ const Home: NextPage<HomePageProps> = ({ data }) => {
           </ContentSectionContainer>
         </Stack>
 
-        <Stack justifyItems='center' textAlign='center' pt={24}>
+        <Stack justifyItems="center" textAlign="center" pt={24}>
           <Text>Create this website with using Next.js and Chakra UI</Text>
           <Box>
             <Text>Contents licensed under CC BY-NC-SA 4.0</Text>
@@ -187,7 +191,7 @@ const Home: NextPage<HomePageProps> = ({ data }) => {
         </Stack>
       </PageContainer>
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
