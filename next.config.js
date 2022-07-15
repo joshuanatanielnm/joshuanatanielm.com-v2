322a30c7522e933cb6772c4b21cf7668ebca70b5
Joshua Nathaniel M <clsjoshua@gmail.com>
2022-07-15 10:55:26 +0700

    feat: add i18n

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
   reactStrictMode: true,
   images: {
-    domains: ['www.datocms-assets.com'],
+    domains: ["www.datocms-assets.com"],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
