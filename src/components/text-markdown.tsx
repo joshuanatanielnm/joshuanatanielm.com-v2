ae50a0617549aa34543f2f4ced31f58a20ef70ab
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2023-10-01 05:21:35 +0700

    feat: update text markdown style

--- src/components/text-markdown.tsx
+++ src/components/text-markdown.tsx
@@ -13,7 +13,7 @@ export const TextMarkdown = ({ children }: TextMarkdownProps) => {
     p: (props: ReactMarkdownProps) => {
       const { children } = props;
       return (
-        <Text fontSize={{ base: "lg", md: "xl" }} color="gray.300">
+        <Text fontSize={{ base: "lg", md: "lg" }} color="gray.300">
           {children}
         </Text>
       );
