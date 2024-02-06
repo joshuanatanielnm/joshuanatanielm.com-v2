76bbb49e6d2ee1c20c4da8fe0c2ffc5c8323e39d
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2024-02-07 00:18:46 +0700

    feat: implement analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../styles/theme";
 import "@fontsource/poppins";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Analytics />
       <Component {...pageProps} />
     </ChakraProvider>
   );
